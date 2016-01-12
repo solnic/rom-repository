f5e014c19bc8b4a7e6ae5da93ddcb1981f07a1b4
Piotr Solnica <piotr.solnica@gmail.com>
2016-01-12 17:50:20 +0100

    Add Repository::Model#update

--- lib/rom/repository/model.rb
+++ lib/rom/repository/model.rb
@@ -8,13 +8,22 @@ module ROM
 
       attr_reader :create_command
 
+      attr_reader :update_command
+
       def initialize(rom)
         super
         @create_command = Commands::Create[adapter].build(relation, result: :one)
+        @update_command = Commands::Update[adapter].build(relation, result: :one)
       end
 
       def create(attributes)
         create_command.call(attributes)
+      end
+
+      def update(pk, attributes)
+        update_command
+          .new(relation.where(relation.primary_key => pk))
+          .call(attributes)
       end
 
       private
--- spec/integration/repository/model_spec.rb
+++ spec/integration/repository/model_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe ROM::Repository::Model do
       expect(user_struct.to_h).to eql(user_tuple)
     end
   end
+
+  describe '#update' do
+    it 'updates existing tuple in a relation' do
+      user_tuple = repo.create(name: 'Jane')
+
+      repo.update(user_tuple[:id], name: 'Jane Doe')
+
+      user_struct = repo.users.where(id: user_tuple[:id]).one
+
+      expect(user_struct.name).to eql('Jane Doe')
+    end
+  end
 end
